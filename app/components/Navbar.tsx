e8b39d99048dd256d4627387d4a786e31c3aea6e
skalez-a <holumacbeth@gmail.com>
2024-09-29 12:17:41 +0100

    home UI

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,13 +1,21 @@
 "use client"
 import { Flex, Box, Icon, Text } from "@chakra-ui/react"
+import { FaWallet } from "react-icons/fa";
 import { FaUserGroup } from "react-icons/fa6"
 import { FaHome } from "react-icons/fa";
-import { FaFire } from "react-icons/fa" 
 import { FaGamepad } from "react-icons/fa"
-// import { Link } from "react-router-dom"
 import Link from "next/link";
 import { BiCoinStack } from "react-icons/bi"
 import { useEffect, useState } from "react"
+import { IconType } from "react-icons";
+
+// Define the props interface for NavItem
+interface NavItemProps {
+  href: string;
+  icon: IconType;
+  label: string;
+  isActive: boolean;
+}
 
 function Navbar({
     userId,
@@ -21,106 +29,95 @@ function Navbar({
   useEffect(() => {
     const path = location.pathname
     if (path == "/") setActiveTab("home")
-    if (path.includes("referral")) setActiveTab("ref")
-    if (path.includes("boost")) setActiveTab("boost") 
-    if (path.includes("tasks")) setActiveTab("tasks")
+    if (path.includes("referral")) setActiveTab("friends")
+    if (path.includes("boost")) setActiveTab("wallet") 
+    if (path.includes("tasks")) setActiveTab("earn")
     if (path.includes("games")) setActiveTab("games")
   }, [])
 
+  const NavItem: React.FC<NavItemProps> = ({ href, icon, label, isActive }) => (
+    <Link href={href} onClick={() => setActiveTab(label.toLowerCase())}>
+  <Box 
+    textAlign="center" 
+    bg={isActive ? "rgba(57, 121, 214, 0.19)" : "transparent"}
+    borderRadius="10px"
+    p={2}
+    transition="all 0.3s"
+    borderWidth={isActive ? "1px" : "0"}
+    borderColor={isActive ? "gray.400" : "transparent"}
+  >
+    <Icon 
+      as={icon} 
+      color={isActive ? "white" : "gray.300"} 
+      boxSize={6} 
+    />
+    <Text 
+      fontSize="xs" 
+      fontWeight="medium" 
+      color={isActive ? "white" : "gray.300"}
+      mt={1}
+    >
+      {label}
+    </Text>
+  </Box>
+</Link>
+  )
+
   return (
     <Flex justify="center">
-     <Box 
-  pos="fixed"
-  display="flex"
-  justifyContent="center" 
-  backdropFilter="blur(2px)"
-  backgroundColor="rgba(11, 11, 11, 0.064)"
-  bottom="0"
-  h="90px"
-  w="100%"
->
+      <Box 
+        pos="fixed"
+        display="flex"
+        justifyContent="center" 
+        backdropFilter="blur(10px)"
+        // backgroundColor="rgba(255, 255, 255, 0.8)"
+        bottom={0}
+        left={0}
+        right={0}
+        h="70px"
+        w="100%"
+      >
         <Flex
-          w={["90%", "360px"]}
-          h="70px" 
-          rounded="20px"
-          // bg="gray.900"
+          w={["100%", "350px"]}
+          maxW="500px"
+          h="100%" 
           justify="space-between"
+          backgroundColor="rgba(35, 35, 35, 1)" 
           align="center"
-          px="2" 
+          px={4}
+          pt={5}
+           borderTopRadius="20px"
         >
-          <Link to={`/boost?userId=${userId}&name=${name}`} onClick={() => setActiveTab("boost")}>
-            <Box textAlign="center">
-            <Box
-                bg={activeTab == "ref" ? "blue.600" : "blue.700"}
-                p="2" 
-                rounded="lg"
-                
-              >
-                
-              <Icon as={FaFire} color={activeTab == "boost" ? "orange.400" : "gray.400"} boxSize="6" />
-              </Box>
-              <Text fontSize="xs" color={activeTab == "boost" ? "white" : "gray.400"}>Boost</Text>
-            </Box>
-          </Link>
-
-          <Link to={`/tasks?userId=${userId}&name=${name}`} onClick={() => setActiveTab("earn")}>
-            <Box textAlign="center">
-            <Box
-                bg={activeTab == "ref" ? "gray.600" : "gray.700"}
-                p="2" 
-                rounded="lg"
-                
-              >
-              <Icon as={BiCoinStack} color={activeTab == "earn" ? "yellow.400" : "gray.400"} boxSize="6" /> 
-              </Box>
-              <Text fontSize="xs" color={activeTab == "earn" ? "white" : "gray.400"}>Earn</Text>
-            </Box>
-          </Link>
-
-          <Link to={`/?userId=${userId}&name=${name}`} onClick={() => setActiveTab("home")}>
-            <Box textAlign="center">
-              <Box 
-                bg={activeTab == "home" ? "gray.600" : "gray.900"}
-                p="2"
-                rounded="lg" 
-                
-                transform="scale(1.2)"
-              >
-                {/* <Image alt="Coin" src="/coin.png" w="40px" h="40px" /> */}
-              <Icon as={FaHome} color={activeTab == "home" ? "purple.400" : "gray.400"} boxSize="10" /> 
-              <Text fontSize="xs" color="white" fontWeight="bold" mt="1">Home</Text>
-              </Box>
-            </Box>
-          </Link>
-
-          <Link to={`#`} onClick={() => setActiveTab("games")}>
-            <Box textAlign="center">
-            <Box
-                bg={activeTab == "ref" ? "gray.600" : "gray.700"}
-                p="2" 
-                rounded="lg"
-                
-              >                
-              <Icon as={FaGamepad} color={activeTab == "games" ? "purple.400" : "gray.400"} boxSize="6" /> 
-              </Box>
-              <Text fontSize="xs" color={activeTab == "games" ? "white" : "gray.400"}>Games</Text>
-            </Box>  
-          </Link>
-
-          <Link to={`/referral?userId=${userId}&name=${name}`} onClick={() => setActiveTab("ref")}>
-            <Box textAlign="center">
-            <Box
-                bg={activeTab == "ref" ? "gray.600" : "gray.700"}
-                p="2" 
-                rounded="lg"
-                
-              >
-                {/* <Icon as={FaUserGroup} color="teal.400" boxSize="6" /> */}
-              <Icon as={FaUserGroup} color={activeTab == "ref" ? "teal.400" : "gray.400"} boxSize="6" />
-              </Box>
-              <Text fontSize="xs" color={activeTab == "ref" ? "white" : "gray.400"}>Refer</Text>
-            </Box>
-          </Link>
+          <NavItem 
+            href={`/referral?userId=${userId}&name=${name}`}
+            icon={FaUserGroup}
+            label="Friends"
+            isActive={activeTab === "friends"}
+          />
+          <NavItem 
+            href={`/tasks`}
+            icon={BiCoinStack}
+            label="Earn"
+            isActive={activeTab === "earn"}
+          />
+          <NavItem 
+            href={`/`}
+            icon={FaHome}
+            label="Home"
+            isActive={activeTab === "home"}
+          />
+          <NavItem 
+            href="#"
+            icon={FaGamepad}
+            label="Games"
+            isActive={activeTab === "games"}
+          />
+          <NavItem 
+            href={`/boost?userId=${userId}&name=${name}`}
+            icon={FaWallet}
+            label="Wallet"
+            isActive={activeTab === "wallet"}
+          />
         </Flex>
       </Box>
     </Flex>
